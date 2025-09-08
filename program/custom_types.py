b7e1d89e85f6ceee04e21d054a3139306a2ff936
donmatthiuz <cor22982@uvg.edu.gt>
2025-09-07 18:20:27 -0600

    sistema de tipos.

--- program/custom_types.py
+++ program/custom_types.py
@@ -25,14 +25,14 @@ class ArrayType(Type):
     def __init__(self, element_type: Type, dimensions: int = 1):
         self.element_type = element_type
         self.dimensions = dimensions
-
+    
     def __str__(self):
         return f"{self.element_type}{'[]' * self.dimensions}"
 
 class ClassType(Type):
     def __init__(self, name: str):
         self.name = name
-
+    
     def __str__(self):
         return self.name
 
@@ -40,7 +40,7 @@ class FunctionType(Type):
     def __init__(self, param_types, return_type: Type):
         self.param_types = param_types  # list of Type
         self.return_type = return_type
-
+    
     def __str__(self):
         params = ", ".join(str(p) for p in self.param_types)
         return f"function({params}): {self.return_type}"
