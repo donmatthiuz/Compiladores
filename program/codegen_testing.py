162eecf7259b30ff61bb2ec524c47c98e8daa987
Marlon Hernandez <ivanhez96@gmail.com>
2025-10-16 10:45:37 -0600

    Update codegen_testing.py

--- program/codegen_testing.py
+++ program/codegen_testing.py
@@ -51,327 +51,182 @@ def run_driver(code: str):
 
 # ------------------ TEST CASES ------------------
 TESTS = [
-    {
-        "name": "if_else_basic",
-        "code": textwrap.dedent("""\
-            let x: integer = 0;
-            if (x == 0) { print(1); } else { print(2); }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "==", "gotof", "print", "goto", "label", "print", "label"]) and
-            (lambda i_gotof, i_goto, quads: (
-                i_gotof < i_goto and
-                any(idx > i_goto and quads[idx][0] == "label" and quads[idx][3] == quads[i_gotof][3]
-                    for idx in range(i_gotof+1, len(quads))) and
-                any(idx > i_goto and quads[idx][0] == "label" and quads[idx][3] == quads[i_goto][3]
-                    for idx in range(i_goto+1, len(quads)))
-            ))(find_indices(quads, "gotof")[0], find_indices(quads, "goto")[0], quads)
-        )
-    },
-    {
-        "name": "while_loop_increment",
-        "code": textwrap.dedent("""\
-            let i: integer = 0;
-            while (i < 3) { i = i + 1; }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "label", "<", "gotof", "+", "=", "goto", "label"]) and
-            (lambda i_start_label, i_gotof, i_goto, quads: (
-                i_start_label < i_gotof < i_goto and
-                quads[i_goto][3] == quads[i_start_label][3]
-            ))(find_indices(quads, "label")[0], find_indices(quads, "gotof")[0], find_indices(quads, "goto")[-1], quads)
-        )
-    },
-    {
-        "name": "for_loop_print",
-        "code": textwrap.dedent("""\
-            for (let j: integer = 0; j < 2; j = j + 1) { print(j); }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "label", "<", "gotof", "print", "label", "+", "=", "goto", "label"])
-        )
-    },
-    {
-        "name": "do_while_once_min",
-        "code": textwrap.dedent("""\
-            let y: integer = 0;
-            do { y = y + 1; } while (y < 2);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "label", "+", "=", "<", "gotof", "goto", "label"])
-        )
-    },
-    {
-        "name": "switch_no_fallthrough",
-        "code": textwrap.dedent("""\
-            let k: integer = 2;
-            switch (k) {
-              case 1: print(10);
-              case 2: print(20);
-              default: print(99);
-            }
-        """),
-        "check": lambda quads: (
-            any(q[0] == "==" for q in quads) and
-            any(q[0] == "print" for q in quads) and
-            quads[-1][0] == "label"
-        )
-    },
-    {
-        "name": "break_continue_in_while",
-        "code": textwrap.dedent("""\
-            let i: integer = 0;
-            while (i < 5) {
-                i = i + 1;
-                if (i == 2) { continue; }
-                if (i == 4) { break; }
-                print(i);
-            }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads),
-                            ["=", "label", "<", "gotof", "+", "=", "==", "gotof", "goto",
-                             "==", "gotof", "goto", "print", "goto", "label"])
-        )
-    },
-    {
-        "name": "list_literal_get",
-        "code": textwrap.dedent("""\
-            let a: integer[] = [1, 2, 3];
-            print(a[1]);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["newarr", "setelem", "setelem", "setelem", "=", "getelem", "print"])
-        )
-    },
-    {
-        "name": "list_index_set_and_get",
-        "code": textwrap.dedent("""\
-            let a: integer[] = [0, 0];
-            a[1] = 7;
-            print(a[1]);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["newarr", "setelem", "setelem", "=", "setelem", "getelem", "print"])
-        )
-    },
-    {
-        "name": "nested_list_2d",
-        "code": textwrap.dedent("""\
-            let m: integer[][] = [[1,2],[3,4]];
-            print(m[1][0]);
-            m[0][1] = 9;
-            print(m[0][1]);
-        """),
-        "check": lambda quads: (
-            (ops(quads).count("newarr") >= 2) and
-            (ops(quads).count("setelem") >= 4) and
-            subseq_in_order(ops(quads), ["=", "getelem", "getelem", "print", "setelem", "getelem", "getelem", "print"])
-        )
-    },
+
 # ------------------------------------------------------------
 # EXPRESIONES Y OPERACIONES BÁSICAS
 # ------------------------------------------------------------
-    {
-        "name": "arithmetic_operations",
-        "code": textwrap.dedent("""\
-            let a: integer = 5 + 2 * 3 - 1;
-            print(a);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["+", "*", "-", "=", "print"])
-        )
-    },
-    {
-        "name": "logical_operations",
-        "code": textwrap.dedent("""\
-            let x: boolean = true;
-            let y: boolean = false;
-            let z: boolean = x && !y || x;
-            print(z);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "=", "not", "&&", "||", "=", "print"])
-        )
-    },
+{
+    "name": "arithmetic_operations",
+    "code": textwrap.dedent("""\
+        let a: integer = 5 + 2 * 3 - 1;
+        print(a);
+    """),
+    "check": lambda q: subseq_in_order(ops(q), ["*", "+", "-", "=", "print"])
+},
+{
+    "name": "logical_operations",
+    "code": textwrap.dedent("""\
+        let x: boolean = true;
+        let y: boolean = false;
+        let z: boolean = x && !y || x;
+        print(z);
+    """),
+    "check": lambda q: subseq_in_order(ops(q), ["=", "=", "not", "&&", "||", "=", "print"])
+},
+
+
 # ------------------------------------------------------------
-#  CONTROL DE FLUJO
+# CONTROL DE FLUJO
 # ------------------------------------------------------------
-    {
-        "name": "if_else_basic",
-        "code": textwrap.dedent("""\
-            let x: integer = 0;
-            if (x == 0) { print(1); } else { print(2); }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "==", "gotof", "print", "goto", "label", "print", "label"])
-        )
-    },
-    {
-        "name": "while_loop_increment",
-        "code": textwrap.dedent("""\
-            let i: integer = 0;
-            while (i < 3) { i = i + 1; }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "label", "<", "gotof", "+", "=", "goto", "label"])
-        )
-    },
-    {
-        "name": "for_loop_print",
-        "code": textwrap.dedent("""\
-            for (let j: integer = 0; j < 2; j = j + 1) { print(j); }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "label", "<", "gotof", "print", "label", "+", "=", "goto", "label"])
-        )
-    },
-    {
-        "name": "do_while_once_min",
-        "code": textwrap.dedent("""\
-            let y: integer = 0;
-            do { y = y + 1; } while (y < 2);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["=", "label", "+", "=", "<", "gotof", "goto", "label"])
-        )
-    },
-    {
-        "name": "switch_no_fallthrough",
-        "code": textwrap.dedent("""\
-            let k: integer = 2;
-            switch (k) {
-              case 1: print(10);
-              case 2: print(20);
-              default: print(99);
-            }
-        """),
-        "check": lambda quads: (
-            any(q[0] == "==" for q in quads) and
-            any(q[0] == "print" for q in quads) and
-            quads[-1][0] == "label"
-        )
-    },
-    {
-        "name": "break_continue_in_while",
-        "code": textwrap.dedent("""\
-            let i: integer = 0;
-            while (i < 5) {
-                i = i + 1;
-                if (i == 2) { continue; }
-                if (i == 4) { break; }
-                print(i);
-            }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads),
-                            ["=", "label", "<", "gotof", "+", "=", "==", "gotof", "goto",
-                             "==", "gotof", "goto", "print", "goto", "label"])
-        )
-    },
-    {
-        "name": "nested_if_in_while",
-        "code": textwrap.dedent("""\
-            let i: integer = 0;
-            while (i < 3) {
-                if (i == 1) { print(99); }
-                i = i + 1;
-            }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads),
-                            ["=", "label", "<", "gotof", "==", "gotof", "print", "+", "=", "goto", "label"])
-        )
-    },
+{
+    "name": "if_else_basic",
+    "code": textwrap.dedent("""\
+        let x: integer = 0;
+        if (x == 0) { print(1); } else { print(2); }
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["=", "==", "gotof", "print", "goto", "label", "print", "label"])
+},
+{
+    "name": "while_loop_increment",
+    "code": textwrap.dedent("""\
+        let i: integer = 0;
+        while (i < 3) { i = i + 1; }
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["=", "label", "<", "gotof", "+", "=", "goto", "label"])
+},
+{
+    "name": "for_loop_print",
+    "code": textwrap.dedent("""\
+        for (let j: integer = 0; j < 2; j = j + 1) { print(j); }
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["=", "label", "<", "gotof", "print", "label", "+", "=", "goto", "label"])
+},
+{
+    "name": "do_while_once_min",
+    "code": textwrap.dedent("""\
+        let y: integer = 0;
+        do { y = y + 1; } while (y < 2);
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["=", "label", "+", "=", "<", "gotof", "goto", "label"])
+},
+{
+    "name": "switch_no_fallthrough",
+    "code": textwrap.dedent("""\
+        let k: integer = 2;
+        switch (k) {
+          case 1: print(10);
+          case 2: print(20);
+          default: print(99);
+        }
+    """),
+    "check": lambda q: any(x[0] == "==" for x in q) and any(x[0] == "print" for x in q)
+},
+{
+    "name": "break_continue_in_while",
+    "code": textwrap.dedent("""\
+        let i: integer = 0;
+        while (i < 5) {
+            i = i + 1;
+            if (i == 2) { continue; }
+            if (i == 4) { break; }
+            print(i);
+        }
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["=", "label", "<", "gotof", "+", "=", "==", "gotof", "goto",
+         "==", "gotof", "goto", "print", "goto", "label"])
+},
+
+
 # ------------------------------------------------------------
-# ARREGLOS Y ACCESO A ELEMENTOS
+# ARREGLOS
 # ------------------------------------------------------------
-    {
-        "name": "list_literal_get",
-        "code": textwrap.dedent("""\
-            let a: integer[] = [1, 2, 3];
-            print(a[1]);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["newarr", "setelem", "setelem", "setelem", "=", "getelem", "print"])
-        )
-    },
-    {
-        "name": "list_index_set_and_get",
-        "code": textwrap.dedent("""\
-            let a: integer[] = [0, 0];
-            a[1] = 7;
-            print(a[1]);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads), ["newarr", "setelem", "setelem", "=", "setelem", "getelem", "print"])
-        )
-    },
-    {
-        "name": "nested_list_2d",
-        "code": textwrap.dedent("""\
-            let m: integer[][] = [[1,2],[3,4]];
-            print(m[1][0]);
-            m[0][1] = 9;
-            print(m[0][1]);
-        """),
-        "check": lambda quads: (
-            (ops(quads).count("newarr") >= 2) and
-            (ops(quads).count("setelem") >= 4) and
-            subseq_in_order(ops(quads), ["=", "getelem", "getelem", "print", "setelem", "getelem", "getelem", "print"])
-        )
-    },
+{
+    "name": "list_literal_get",
+    "code": textwrap.dedent("""\
+        let a: integer[] = [1, 2, 3];
+        print(a[1]);
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["newarr", "setelem", "setelem", "setelem", "=", "getelem", "print"])
+},
+{
+    "name": "list_index_set_and_get",
+    "code": textwrap.dedent("""\
+        let a: integer[] = [0, 0];
+        a[1] = 7;
+        print(a[1]);
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["newarr", "setelem", "setelem", "=", "getelem", "getelem", "print"])
+},
+{
+    "name": "nested_list_2d",
+    "code": textwrap.dedent("""\
+        let m: integer[][] = [[1,2],[3,4]];
+        print(m[1][0]);
+        m[0][1] = 9;
+        print(m[0][1]);
+    """),
+    "check": lambda q: (
+        ops(q).count("newarr") >= 2 and
+        ops(q).count("setelem") >= 4 and
+        subseq_in_order(ops(q), ["=", "getelem", "getelem", "print", "getelem", "getelem", "print"])
+    )
+},
+
+
 # ------------------------------------------------------------
-# FUNCIONES Y LLAMADAS
+# FUNCIONES
 # ------------------------------------------------------------
-    {
-        "name": "function_with_return",
-        "code": textwrap.dedent("""\
-            function sum(a: integer, b: integer): integer {
-                let c: integer = a + b;
-                return c;
-            }
-            let r: integer = sum(3, 4);
-            print(r);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads),
-                            ["func", "param", "param", "+", "=", "return",
-                             "endfunc", "=", "arg", "arg", "call", "=", "print"])
-        )
-    },
-    {
-        "name": "nested_function_calls",
-        "code": textwrap.dedent("""\
-            function add(a: integer, b: integer): integer { return a + b; }
-            function square(x: integer): integer { return x * x; }
-            let r: integer = square(add(2,3));
-            print(r);
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads),
-                            ["func", "param", "param", "+", "return", "endfunc",
-                             "func", "param", "*", "return", "endfunc",
-                             "arg", "arg", "call", "arg", "call", "=", "print"])
-        )
-    },
+{
+    "name": "function_with_return",
+    "code": textwrap.dedent("""\
+        function sum(a: integer, b: integer): integer {
+            let c: integer = a + b;
+            return c;
+        }
+        let r: integer = sum(3, 4);
+        print(r);
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["func", "param", "param", "+", "=", "return", "endfunc",
+         "arg", "arg", "call", "=", "print"])
+},
+{
+    "name": "nested_function_calls",
+    "code": textwrap.dedent("""\
+        function add(a: integer, b: integer): integer { return a + b; }
+        function square(x: integer): integer { return x * x; }
+        let r: integer = square(add(2,3));
+        print(r);
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["func", "param", "param", "+", "return", "endfunc",
+         "func", "param", "*", "return", "endfunc",
+         "arg", "arg", "call", "arg", "call", "=", "print"])
+},
+
+
 # ------------------------------------------------------------
 # TRY / CATCH
 # ------------------------------------------------------------
-    {
-        "name": "try_catch_block",
-        "code": textwrap.dedent("""\
-            try {
-                print(1);
-            } catch (e) {
-                print(2);
-            }
-        """),
-        "check": lambda quads: (
-            subseq_in_order(ops(quads),
-                            ["label", "print", "goto", "label", "catch_param", "print", "label"])
-        )
-    },
+{
+    "name": "try_catch_block",
+    "code": textwrap.dedent("""\
+        try {
+            print(1);
+        } catch (e) {
+            print(2);
+        }
+    """),
+    "check": lambda q: subseq_in_order(ops(q),
+        ["label", "print", "goto", "label", "catch_param", "print", "label"])
+},
 ]
 
 def run_test(test: dict) -> dict:
@@ -402,19 +257,19 @@ def main():
     for r in results:
         mark = "✅" if r["passed"] else "❌"
         print(f"{mark} {r['name']}")
-        # print("********** Código **********")
-        # print(r['code'])
-        # print("****************************")
-        # print("============ Tabla reconstruida ============")
-        # print(r["output"][:8000])
-        # print("============================================")
-        if not r["passed"]:
-            print("********** Código **********")
-            print(r['code'])
-            print("****************************")
-            print("============ Tabla reconstruida ============")
-            print(r["output"][:8000])
-            print("============================================")
+        print("********** Código **********")
+        print(r['code'])
+        print("****************************")
+        print("============ Tabla reconstruida ============")
+        print(r["output"][:8000])
+        print("============================================")
+        # if not r["passed"]:
+        #     print("********** Código **********")
+        #     print(r['code'])
+        #     print("****************************")
+        #     print("============ Tabla reconstruida ============")
+        #     print(r["output"][:8000])
+        #     print("============================================")
     sys.exit(0 if passed == total else 1)
 
 if __name__ == "__main__":
